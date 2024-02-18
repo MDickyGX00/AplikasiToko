3a48da7f5dad0ea29fc10c9bc72ee8030d3ce09b
ZeroGX <73316891+MDickyGX00@users.noreply.github.com>
2024-02-18 19:55:50 +0700

    Add files via upload

---
+++ AplikasiToko/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            TextBox2.UseSystemPasswordChar = True
+        Else
+            TextBox2.UseSystemPasswordChar = False
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" Or TextBox2.Text = "" Then
+            MsgBox("Username / Password Kosong")
+        ElseIf TextBox1.Text = "admin" And TextBox2.Text = "pass" Then
+            Form2.Show()
+            Me.Hide()
+        Else
+            MsgBox("Username dan Password Salah")
+        End If
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+End Class
---
+++ AplikasiToko/yellow.Designer.vb
@@ -0,0 +1,55 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class yellow
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.RoyalBlue
+        Me.Label4.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label4.Location = New System.Drawing.Point(53, 147)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(267, 22)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Ini adalah Form Stok Barang"
+        '
+        'yellow
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.RoyalBlue
+        Me.ClientSize = New System.Drawing.Size(369, 334)
+        Me.Controls.Add(Me.Label4)
+        Me.Name = "yellow"
+        Me.Text = "yellow"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label4 As Label
+End Class
